efcdae18a5b60d4a7fa268a15f72f482e12245ba
GadourGuy <ogadours1@example.com>
2024-05-19 21:45:01 +0800

    adding task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """Returns Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Returns HBNB"""
-    return "HBNB"
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
